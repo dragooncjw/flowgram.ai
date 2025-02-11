2d8824e625956f4016cd921795d01cf43b1198c0
chenjiawei.inizio <chenjiawei.inizio@bytedance.com>
2025-02-11 16:47:06 +0800

    chore: docs use CSR instead of SSR

--- apps/docs/components/fixed-feature-overview/index.tsx
+++ apps/docs/components/fixed-feature-overview/index.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-
 import './index.less';
 
-// https://github.com/web-infra-dev/rspress/issues/553
-const FixedFeatureOverview = React.lazy(() =>
-  import('@flowgram.ai/demo-fixed-layout').then((module) => ({
-    default: module.DemoFixedLayout,
-  }))
-);
-
-export { FixedFeatureOverview };
+export { DemoFixedLayout as FixedFeatureOverview } from '@flowgram.ai/demo-fixed-layout';
--- apps/docs/components/free-feature-overview/index.tsx
+++ apps/docs/components/free-feature-overview/index.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
 import './index.less';
 
-// https://github.com/web-infra-dev/rspress/issues/553
-const FreeFeatureOverview = React.lazy(() =>
-  import('@flowgram.ai/demo-free-layout').then((module) => ({
-    default: module.DemoFreeLayout,
-  }))
-);
-
-export { FreeFeatureOverview };
+export { DemoFreeLayout as FreeFeatureOverview } from '@flowgram.ai/demo-free-layout';
