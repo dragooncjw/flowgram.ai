ea5b0459485479b05093d349481ee455c421ff00
dragooncjw <289056872@qq.com>
2025-07-01 16:45:22 +0800

    chore: add license header

--- packages/materials/form-materials/src/components/variable-selector/use-variable-tree.tsx
+++ packages/materials/form-materials/src/components/variable-selector/use-variable-tree.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+ * SPDX-License-Identifier: MIT
+ */
+
 import React, { useCallback } from 'react';
 
 import { useScopeAvailable, ASTMatch, BaseVariableField } from '@flowgram.ai/editor';
